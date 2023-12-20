1593e1f0d12ac7ecfb102cbcacdee0cde282eb93
James <jameskimanzi@gmail.com>
2023-12-21 02:30:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const companyName= 'Scuber';
+
+if (/Scuber/.test(companyName)) {
+  console.log('Company name matches the pattern');
+} else {
+  console.log('Company name does not match the pattern');
+}
+
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo;
+companyCeo = 'Susan Smith';
